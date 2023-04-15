1e74a1495df0f2922b50c89c5913ec0d796eee2c
Pavel-113 <yartsev87@gmail.com>
2023-04-15 13:14:00 +0700

    program is job!

--- task1/src/main/java/ru/cft/shift/Table.java
+++ task1/src/main/java/ru/cft/shift/Table.java
@@ -2,6 +2,17 @@ package ru.cft.shift;
 
 public class Table {
 
+    private StringBuilder LineSb = new StringBuilder();
+    private int countDigit_a;
+    private int countDigit_b;
+    private int countDigit_ab;
+
+
+    void getCountDigit (int a, int b){
+        countDigit_a = getNUm(a);
+        countDigit_b = getNUm(b);
+        countDigit_ab = getNUm(a * b);
+    }
     int getNUm(int number) {
         int numbersCount = 0;
         do {
@@ -15,48 +26,43 @@ public class Table {
         System.out.print(symbol);
     }
 
-    void printVertBar() {
-        System.out.print("|");
-    }
-
-    void printSpace() {
-        System.out.print(" ");
+    void getLine(int a, int b) {
+        int len = b * (getNUm(a * b) + 1) - getNUm(b);
+        int k = len + getNUm(a) - 1;
+        for (int i = 0; i < getNUm(b); i++) LineSb.append("-");
+        LineSb.append("+");
+        while(k != 0) {
+            if (k % (getNUm(a * b) + 1) != 0) {
+                LineSb.append("-");
+            } else {
+                LineSb.append("+");
+            }
+            k--;
+        }
     }
 
     void getMulTable(int a, int b) {
-        int len = b* (getNUm(a * b) + 1) - getNUm(b);
+        getLine(a, b);
+        getCountDigit(a, b);
         for (int i = 0; i <= a; i++) {
             for (int j = 0; j <= b; j++) {
+
                 if (j*i != 0) {
-                    for (int z = 0; z < getNUm(a * b) - getNUm(i * j); z++) printSpace();
+                    for (int z = 0; z < getNUm(a * b) - getNUm(i * j); z++) printSymbol(" ");
                     System.out.print(i * j);
-                    if (j != b) printVertBar();
+                    if (j != b) printSymbol("|");
                 } else {
-                    if (j != 0) for (int z = 0; z < getNUm(a * b) - getNUm(1 * j); z++) printSpace();
+                    if (j != 0) for (int z = 0; z < getNUm(a * b) - getNUm(1 * j); z++) printSymbol(" ");
                     if (j != 0) {
                         System.out.print(j);
                     } else {
-                        for (int z = 0; z < getNUm(a) - getNUm(i * 1); z++) printSpace();
+                        for (int z = 0; z < getNUm(a) - getNUm(i * 1); z++) printSymbol(" ");
                         System.out.print(i);
                     }
-                    if (j != b) printVertBar();
+                    if (j != b) printSymbol("|");
                 }
             }
-
-            System.out.print("\n");
-            for (int z = 0; z < getNUm(b); z++) printSymbol("-");
-            printSymbol("+-");
-            int tmp  = len;
-            while(tmp != 0) {
-                if (tmp % (getNUm(a * b) + 1) != 0) {
-                    printSymbol("-");
-                } else {
-                    printSymbol("+");
-                }
-//                System.out.print(len);
-                tmp--;
-            }
-            System.out.print("\n");
+            System.out.print("\n" + LineSb + "\n");
         }
     }
 
